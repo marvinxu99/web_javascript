7c9744f3641088030fcb24ec23578f2f7946d73f
Marvin Xu <marvinxu99@hotmail.com>
2021-06-05 18:57:23 -0700

    added for await of example

--- Advanced JavaScript/Async/promise_jsonholder.js
+++ Advanced JavaScript/Async/promise_jsonholder.js
@@ -45,6 +45,9 @@ function fetchData() {
 			.catch(function (e){
 				console.log(e.message);
 			})
+			.finally(() => {
+				console.log("Finally...");
+			})
 	));
 }
 
@@ -65,10 +68,42 @@ const fetchDataAsync = async function() {
 		console.log('albumss', albums);
 		console.log('posts', posts);
 		
-	} catch(e) {
-		console.log("oops", e);	
-	}
+	} catch(err) {
+		console.log("oops", err);	
+	} finally {
+		console.log("Finally....");
+	}	
 }
 
+// for of 
+function loopThrough() {
+    const array1 = [1,2,3,4,5];
+    for (arr of array1) {
+        console.log(arr);
+    }
+}
+loopThrough();
+
+/* With async and for await of */
+const fetchDataAsync2 = async function() {
+	const urls = [
+		'https://jsonplaceholder.typicode.com/users',
+		'https://jsonplaceholder.typicode.com/albums',
+		'https://jsonplaceholder.typicode.com/posts'
+	]
+	
+	try {
+		const arrayOfPromises = urls.map(url => fetch(url));
+		for await (let request of arrayOfPromises) {
+			const data = await request.json();	
+			console.log(data);
+		}
+		
+	} catch(err) {
+		console.log("oops", err);	
+	} finally {
+		console.log("Finally....");
+	}	
+}
 
 
